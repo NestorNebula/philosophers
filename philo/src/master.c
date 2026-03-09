79ce530f6b4eb29bf1fde86ce7534999bd811ac1
Noa Houssier <nestornebula.dev@gmail.com>
2026-03-09 09:12:15 +0100

    feat: add init functions definitions

---
+++ philo/src/master.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   master.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 08:30:13 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/03/09 08:31:15 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "master.h"
+
+int	init_master(t_master *master, size_t args_size, char **args)
+{
+	(void) master;
+	(void) args_size;
+	(void) args;
+	return (0);
+}
+
+int	clear_master(t_master *master)
+{
+	(void) master;
+	return (0);
+}
---
+++ philo/src/philo.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 08:31:23 by nhoussie          #+#    #+#             */
+/*   Updated: 2026/03/09 08:32:25 by nhoussie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	init_philo(t_philo *philo, unsigned int number, t_master *philo_master)
+{
+	(void) philo;
+	(void) number;
+	(void) philo_master;
+	return (0);
+}
+
+int	clear_philo(t_philo *philo)
+{
+	(void) philo;
+	return (0);
+}
