9f7e68b82bca9a338737cf9c73dbfd5cc5ff85b6
Noa Houssier <nestornebula.dev@gmail.com>
2026-03-11 17:21:56 +0100

    fix: resolve issues in philo's initialization
    Initialize mutex before thread
    Destroy mutex after thread join

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,14 +6,12 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 08:31:23 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/03/09 08:32:25 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/03/10 10:57:50 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include "philo.h"
-
-static void	*philo_routine(void *arg);
 
 int	init_philo(t_philo *philo, unsigned int number, t_master *philo_master)
 {
@@ -34,27 +32,19 @@ int	init_philo(t_philo *philo, unsigned int number, t_master *philo_master)
 	if (pthread_mutex_lock(&philo->context->mutex) != 0)
 		return (1);
 	rc = 0;
-	if (pthread_create(&philo->thread, NULL, philo_routine, NULL) != 0)
+	if (pthread_mutex_init(&philo->mutex, NULL) != 0)
 		rc = 1;
-	else if (pthread_mutex_init(&philo->mutex, NULL) != 0)
+	else if (pthread_create(&philo->thread, NULL, philo_routine, philo) != 0)
 		rc = 1;
-	if (rc != 0)
-	{
+	if (rc == 1)
 		philo->context->running = 0;
-		pthread_join(philo->thread, NULL);
-	}
 	pthread_mutex_unlock(&philo->context->mutex);
 	return (rc);
 }
 
 int	clear_philo(t_philo *philo)
 {
-	pthread_mutex_destroy(&philo->mutex);
 	pthread_join(philo->thread, NULL);
+	pthread_mutex_destroy(&philo->mutex);
 	return (0);
-}
-
-static void	*philo_routine(void *arg)
-{
-	return (arg);
 }
