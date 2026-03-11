47eb0d0a55344ca7dfb13a130c324a7aa4d2eb3f
Noa Houssier <nestornebula.dev@gmail.com>
2026-03-11 17:23:02 +0100

    feat: add master_routine function prototype

--- philo/include/master.h
+++ philo/include/master.h
@@ -6,7 +6,7 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 08:31:47 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/03/08 08:41:33 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/03/10 09:28:20 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,15 @@
  * @param args An array storing the arguments to use for master initialization
  * @return 0 on success, > 0 on error
  */
-int	init_master(t_master *master, size_t args_size, char **args);
+int		init_master(t_master *master, size_t args_size, char **args);
+
+/**
+ * The routine function for a master.
+ *
+ * @param arg A pointer to a master structure
+ * @return A null pointer
+ */
+void	*master_routine(void *arg);
 
 /**
  * Clears the content inside a master structure.
@@ -31,6 +39,6 @@ int	init_master(t_master *master, size_t args_size, char **args);
  * @param master A pointer to the master structure to clear
  * @return 0 on success, > 0 on error
  */
-int	clear_master(t_master *master);
+int		clear_master(t_master *master);
 
 #endif // !MASTER_H
